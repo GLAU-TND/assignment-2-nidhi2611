158efa296ad3b6021c79c945c9264f20f56092cb
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2020-03-25 23:45:56 +0530

    feilds defined

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -1,34 +1,8 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:06 PM
- */
 package problem5.node;
-// to define node properties
+
+import problem5.student.Student;
+
 public class Node {
-    private Node rear;
-    private int size;
-
-    public Node() {
-        rear = null;
-        size = 0;
-
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    public Node getRear() {
-        return rear;
-    }
-
-    public void setRear(Node rear) {
-        this.rear = rear;
-    }
-
-    public void setSize(int size) {
-        this.size = size;
-    }
+    private Student data;
+    private Node next;
 }
