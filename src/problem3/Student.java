a6e31acdb39990eaf344d7f4b7ec3bf64a9dc968
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2020-03-25 18:59:31 +0530

    constructor defined for student class

--- src/problem3/Student.java
+++ src/problem3/Student.java
@@ -3,4 +3,9 @@ package problem3;
 public class Student {
     private String name;
     private int rollNo;
+
+    public Student(String s, int r) {
+        name = s;
+        rollNo = r;
+    }
 }
