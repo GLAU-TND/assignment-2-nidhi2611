549b7bd80f8b7edd244802e7f892019d3ae74aa7
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2020-03-25 20:24:44 +0530

    Scanner class imported

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -6,12 +6,19 @@
  */
 package problem3.main;
 
+import problem3.Student;
 import problem3.myqueue.MyPriorityQueue;
+
+import java.util.Scanner;
 
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
     public static void main(String[] args) {
         MyPriorityQueue queue = new MyPriorityQueue();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Student name");
+
+        Student student = new Student()
     }
 }
