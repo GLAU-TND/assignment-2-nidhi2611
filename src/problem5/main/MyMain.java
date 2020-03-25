e4d9bfcdf6b731e9c027af7084a0e3fecdb2c956
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2020-03-26 00:09:03 +0530

    function called

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -6,6 +6,8 @@
  */
 package problem5.main;
 
+import problem5.student.Student;
+
 import java.util.Scanner;
 
 //executable class
@@ -14,5 +16,12 @@ public class MyMain {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of Student list");
         int size = sc.nextInt();
+        for (int i = 0; i < size; i++) {
+            System.out.println("ENTER THE STUDENT NAME");
+            String s = sc.nextLine();
+            System.out.println("ENTER THE NUMBER OF BACKLOGS");
+            int b = sc.nextInt();
+            Student student = new Student(s, b);
+        }
     }
 }
