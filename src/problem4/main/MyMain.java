43f80402dfc85c494c6ba3904ba732f156a77dd5
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2020-03-28 01:39:41 +0530

    Scanner class imported

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -5,7 +5,20 @@
  *  Time: 7:08 PM
  */
 package problem4.main;
+
+import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
+import java.util.Scanner;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        MyQueue queue = new MyQueue();
+        System.out.println("no of elements you want to add");
+        Scanner sc = new Scanner(System.in);
+
+    }
 }
