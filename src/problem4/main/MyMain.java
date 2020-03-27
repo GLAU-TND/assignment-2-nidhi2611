f8ebebca8cf215147ab3fb7c4bf21b5dca63ed2e
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2020-03-28 02:20:01 +0530

    object created

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -1,9 +1,17 @@
 package problem4.main;
 
+import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
 import java.util.Scanner;
 
 
 public class MyMain {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        MyBinarySearchTree m = new MyBinarySearchTree();
+        MyQueue q = new MyQueue();
+
+
     }
+}
