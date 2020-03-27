5c690c1aab06da10ee5d457e04738fc562106596
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2020-03-28 01:23:17 +0530

    setter defined

--- src/problem4/myqueue/Node.java
+++ src/problem4/myqueue/Node.java
@@ -16,4 +16,12 @@ public class Node {
     public Node getNext() {
         return next;
     }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
 }
