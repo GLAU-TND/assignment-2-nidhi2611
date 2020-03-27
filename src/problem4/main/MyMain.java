b277e289067234171c2dc7c92a854c99f4eb04c6
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2020-03-28 02:18:10 +0530

    clases imported

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -1,27 +1,5 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:08 PM
- */
 package problem4.main;
 
-import problem1.mybst.MyBinarySearchTree;
-
-import java.util.Scanner;
-
-// executable class
-// use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
-    public static void main(String[] args) {
-        MyBinarySearchTree tree = new MyBinarySearchTree();
-        MyQueue queue = new MyQueue();
-        System.out.println("no of elements you want to add");
-        Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < sc.nextInt(); i++) {
-            tree.add(sc.nextInt());
-        }
-        queue.enqueue(tree.root);
-        queue.printQueue();
-    }
+
 }
