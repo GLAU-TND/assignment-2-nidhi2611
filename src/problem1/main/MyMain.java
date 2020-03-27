0906cf4d0119bf20336fc8bffb400fc734b39bed
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2020-03-28 00:57:35 +0530

    main function written

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,21 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
+import java.util.Scanner;
+
 public class MyMain {
+    public static void main(String args[]) {
+        MyBinarySearchTree myBinarySearchTree = new MyBinarySearchTree();
+        Scanner sc = new Scanner(System.in);
+        myBinarySearchTree.add(sc.nextInt());
+        myBinarySearchTree.add(sc.nextInt());
+        myBinarySearchTree.add(sc.nextInt());
+        myBinarySearchTree.add(sc.nextInt());
+        myBinarySearchTree.add(sc.nextInt());
+        myBinarySearchTree.add(sc.nextInt());
+        myBinarySearchTree.add(sc.nextInt());
+
+    }
 }
