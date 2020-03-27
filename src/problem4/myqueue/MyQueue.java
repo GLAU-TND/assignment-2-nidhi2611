93ddac525e80a63b5eed50cd380b17971fb3e590
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2020-03-28 01:34:16 +0530

    enqueue completed

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -7,6 +7,11 @@
 package problem4.myqueue;
 // to create queue to store pre - order successor
 
+import problem1.node.TreeNode;
+
+import static problem1.mybst.MyBinarySearchTree.preOrder;
+import static problem1.mybst.MyBinarySearchTree.preOrderString;
+
 public class MyQueue {
         private Node rear;
         private Node front;
@@ -18,4 +23,20 @@ public class MyQueue {
                 size = 0;
         }
 
+        public void enqueue(TreeNode root) {
+                preOrder(root);
+                String[] data = preOrderString.toString().split(" ");
+                for (int i = 0; i < data.length; i++) {
+                        Node node = new Node(Integer.parseInt(data[i]));
+                        if (front == null) {
+                                rear = node;
+                                front = node;
+                                size++;
+                        } else {
+                                rear.setNext(node);
+                                rear = node;
+                                size++;
+                        }
+                }
+        }
 }
